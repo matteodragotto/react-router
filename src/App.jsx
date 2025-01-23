3910ce5455a85edbb44db4a7861ac592a1e09be8
matteodragotto <matteodrag8@gmail.com>
2025-01-23 14:59:06 +0100

    [feat] routing on app entry point

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import HomePage from "./pages/HomePage"
+import PostsPage from "./pages/PostsPage"
+import AboutPage from "./pages/AboutPage"
+import ContactsPage from "./pages/ContactsPage"
+import DefaultLayout from "./layouts/DefaultLayout"
+
+
+
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route Component={DefaultLayout}>
+          <Route path="/" Component={HomePage} />
+          <Route path="/articoli" Component={PostsPage} />
+          <Route path="/chi-siamo" Component={AboutPage} />
+          <Route path="/contatti" Component={ContactsPage} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -10,6 +10,7 @@ const HomePage = () => {
         </div>
       </div>
     </div>
+  )
 }
 
 export default HomePage
