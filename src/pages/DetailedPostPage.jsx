9590ba57990292ba38913cbc77ae53c63a775b35
matteodragotto <matteodrag8@gmail.com>
2025-01-24 12:28:09 +0100

    [feat] detailed post page now shows the correct post

--- src/pages/DetailedPostPage.jsx
+++ src/pages/DetailedPostPage.jsx
@@ -1,22 +1,58 @@
+import axios from "axios"
+import { useParams, useNavigate } from "react-router-dom"
+import { useState, useEffect } from "react"
 
 const DetailedPostPage = () => {
+
+  const { id } = useParams()
+  const postApiUrl = 'http://localhost:3001'
+  const navigate = useNavigate()
+  const [post, setPost] = useState(null)
+
+  const fetchPost = () => {
+    axios.get(`${postApiUrl}/posts/${id}`)
+      .then(res => {
+        setPost(res.data)
+      })
+      .catch(error => {
+        console.error('Errore durante il caricamento dei post:', error)
+      })
+
+  }
+
+
+  const deletePostHandler = () => {
+    axios.delete(`${postApiUrl}/posts/${id}`)
+      .then(res => {
+        fetchPost()
+      })
+      .catch(error => {
+        console.error('Non è stato possibile cancellare il post:', error)
+      })
+
+  }
+
+  useEffect(() => {
+    fetchPost()
+  }, [])
+
   return (
     <div className="container my-5">
       <h1 className="text-center">Il mio blog</h1>
       <div className="row">
-        <div className="col-12" key={post.id}>
-          <div className="card h my-2">
-            <img src={post.image} alt={post.title} />
+        <div className="col-12" key={post?.id}>
+          <div className="card my-2">
+            <img src={post?.image} alt={post?.title} />
             <div className="card-body text-overflow">
               <h5 className="card-text">
-                {post.title}
+                {post?.title}
               </h5>
               <p className="card-text">
-                {post.content}
+                {post?.content}
               </p>
               <h6>Tag:</h6>
-              <p className="card-text">{post.tags.join(', ')}</p>
-              <div className="btn btn-danger" onClick={() => deletePostHandler(post.id)}>
+              <p className="card-text">{post?.tags.join(', ')}</p>
+              <div className="btn btn-danger" onClick={deletePostHandler}>
                 <i className="fa-solid fa-trash-can"></i>
               </div>
             </div>
